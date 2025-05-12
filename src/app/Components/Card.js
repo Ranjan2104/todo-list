7b4a7cd79bcde4a570740166898b651cd5df8076
Ranjan2104 <ranjan.amresh100@gmail.com>
2025-05-12 08:50:59 +0530

    checkbox added

--- src/app/Components/Card.js
+++ src/app/Components/Card.js
@@ -1,21 +1,44 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { deleteData, getAllData } from "../utils/IndexDB";
 
 const Card = ({ sNo, ele, setTodos }) => {
-  const handleDetele = async (id) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleDelete = async (id) => {
     await deleteData("MyDB", "todos", id);
     getAllData("MyDB", "todos").then(setTodos || []);
   };
+
   return (
-    <div className="card-main">
-      <div>{`${sNo} - ${ele.text}`}</div>
-      <div style={{ cursor: "pointer" }}>
+    <div className="card-main" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+      <div style={{ textDecoration: checked ? "line-through" : "none", wordBreak: "break-word", flex: 1 }}>
+        {`${sNo} - ${ele.text}`}
+      </div>
+      <div
+        style={{
+          cursor: "pointer",
+          display: "flex",
+          gap: 10,
+          alignItems: "center",
+        }}
+      >
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+          style={{
+            cursor: "pointer",
+            width: 20,
+            height: 20,
+            margin: 0,
+          }}
+        />
         <img
           src="https://img.icons8.com/?size=100&id=KtLdJpNivpcV&format=png&color=000000"
           height={25}
-          onClick={() => handleDetele(ele.id)}
+          onClick={() => handleDelete(ele.id)}
         />
       </div>
     </div>
